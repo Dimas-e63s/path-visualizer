52d6db6ecfb381d30ddc474d32309e719eaf82a6
Dimas-e63s <Cyber_cat_2002@mail.ru>
2022-01-17 20:18:55 +0100

    build(RWD): add taiga-ui dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TuiButtonModule, TuiDataListModule, TuiHostedDropdownModule, TuiRootModule, TuiSvgModule} from '@taiga-ui/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { GridNodeComponent } from './grid-node/grid-node.component';
+import {AppComponent} from './app.component';
+import {GridNodeComponent} from './grid-node/grid-node.component';
+import {HeaderComponent} from './header/header.component';
+import {DropdownComponent} from './dropdown/dropdown.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GridNodeComponent,
+    HeaderComponent,
+    DropdownComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TuiRootModule,
+    BrowserAnimationsModule,
+    TuiButtonModule,
+    TuiDataListModule,
+    TuiHostedDropdownModule,
+    TuiSvgModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
