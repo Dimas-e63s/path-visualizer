489ea737f45db022cddd6a5d007012e2c097a1a5
Dimas-e63s <Cyber_cat_2002@mail.ru>
2022-02-13 17:35:08 +0100

    feat(legend): add legend component

---
+++ src/app/legend/legend.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'app-legend',
+  templateUrl: './legend.component.html',
+  styleUrls: ['./legend.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class LegendComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
