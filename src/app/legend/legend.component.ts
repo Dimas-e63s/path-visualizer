b27cb88316deffde573a07a8c261f1ad02faff67
Dimas-e63s <Cyber_cat_2002@mail.ru>
2022-02-15 18:13:45 +0100

    refactor(legend): add generation of legend items

--- src/app/legend/legend.component.ts
+++ src/app/legend/legend.component.ts
@@ -1,16 +1,56 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, ViewEncapsulation} from '@angular/core';
+import {DescriptionFactory} from './notification';
+import {PathAlgorithmEnum} from '../header/header.component';
 
 @Component({
   selector: 'app-legend',
   templateUrl: './legend.component.html',
   styleUrls: ['./legend.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LegendComponent implements OnInit {
+export class LegendComponent {
+  @Input() selectedAlgorithm!: PathAlgorithmEnum | null;
 
-  constructor() { }
+  legendItems = [
+    {
+      className: '--path',
+      description: 'Shortest-path Node'
+    },
+    {
+      className: '--wall',
+      description: 'Wall Node'
+    },
+    {
+      className: '--visited',
+      description: 'Visited Node'
+    },
+    {
+      className: '--unvisited',
+      description: 'Unvisited Node'
+    }
+  ]
 
-  ngOnInit(): void {
+  icons = [
+    {
+      className: '--start',
+      description: 'Start Node'
+    },
+    {
+      className: '--target',
+      description: 'Target Node'
+    },
+    {
+      className: '--weight',
+      description: 'Weight Node'
+    },
+    {
+      className: '--bomb',
+      description: 'Bomb Node'
+    }
+  ];
+
+  getNotificationDescription(): string {
+    return DescriptionFactory.main(this.selectedAlgorithm);
   }
-
 }
