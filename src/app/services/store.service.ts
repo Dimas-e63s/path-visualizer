d6ace11ff6637a17c758c7513968d4b9a277fd86
Dimas-e63s <Cyber_cat_2002@mail.ru>
2022-02-20 19:35:54 +0100

    refactor(store-service): add functions for update and select start/end Nodes

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,11 +1,35 @@
 import { Injectable } from '@angular/core';
 
+interface NodeCoordinates {
+  rowIdx: number;
+  colIdx: number;
+}
+
+// TODO:
+// - limit injector scope only to app component
+
 @Injectable({
   providedIn: 'root'
 })
 export class StoreService {
-  startNode = {colIdx: 2, rowIdx: 25};
-  finishNode = {colIdx: 25, rowIdx: 0};
+  private startNode = {colIdx: 2, rowIdx: 25};
+  private endNode = {colIdx: 25, rowIdx: 0};
 
   constructor() { }
+
+  getStartNode(): NodeCoordinates {
+    return this.startNode;
+  }
+
+  getEndNode(): NodeCoordinates {
+    return this.endNode;
+  }
+
+  updateStartNode(coordinates: NodeCoordinates): void {
+    this.startNode = coordinates;
+  }
+
+  updateEndNode(coordinates: NodeCoordinates): void {
+    this.endNode = coordinates;
+  }
 }
